fa10a70a8f25932718a279bbb658423e71c0dd75
Julien Palard <julien@palard.fr>
2020-10-05 20:36:19 +0200

    Hello 3.9.0!

--- build_docs.py
+++ build_docs.py
@@ -103,7 +103,7 @@ VERSIONS = [
     Version("3.6", "3.6", "security-fixes", sphinx_version="2.3.1"),
     Version("3.7", "3.7", "security-fixes", sphinx_version="2.3.1"),
     Version("3.8", "3.8", "stable", sphinx_version="2.4.4"),
-    Version("3.9", "3.9", "pre-release", sphinx_version="2.4.4"),
+    Version("3.9", "3.9", "stable", sphinx_version="2.4.4"),
     Version("3.10", "master", "in development", sphinx_version="3.2.1"),
 ]
 
