f3a655aeb42c8c6db1214ed03f781de718eb9140
Adam Turner <9087854+aa-turner@users.noreply.github.com>
2024-08-17 13:39:17 +0100

    Update README tables

--- check_versions.py
+++ check_versions.py
@@ -13,6 +13,8 @@ import git
 import build_docs
 
 logger = logging.getLogger(__name__)
+VERSIONS = build_docs.parse_versions_from_devguide()
+LANGUAGES = build_docs.parse_languages_from_config()
 
 
 def parse_args():
@@ -24,102 +26,107 @@ def parse_args():
     return parser.parse_args()
 
 
-def remote_by_url(repo: git.Repo, url_pattern: str):
+def find_upstream_remote_name(repo: git.Repo) -> str:
     """Find a remote of repo matching the regex url_pattern."""
     for remote in repo.remotes:
         for url in remote.urls:
-            if re.search(url_pattern, url):
-                return remote
+            if "github.com/python" in url:
+                return f"{remote.name}/"
 
 
 def find_sphinx_spec(text: str):
     if found := re.search(
-        """sphinx[=<>~]{1,2}[0-9.]{3,}|needs_sphinx = [0-9.'"]*""", text, flags=re.I
+        """sphinx[=<>~]{1,2}[0-9.]{3,}|needs_sphinx = [0-9.'"]*""",
+        text,
+        flags=re.IGNORECASE,
     ):
         return found.group(0).replace(" ", "")
     return "ø"
 
 
-def find_sphinx_in_file(repo: git.Repo, branch, filename):
-    upstream = remote_by_url(repo, "github.com.python").name
-    # Just in case you don't use origin/:
-    branch = branch.replace("origin/", upstream + "/")
-    try:
-        return find_sphinx_spec(repo.git.show(f"{branch}:{filename}"))
-    except git.exc.GitCommandError:
-        return "ø"
+def find_sphinx_in_files(repo: git.Repo, branch_or_tag, filenames):
+    upstream = find_upstream_remote_name(repo)
+    # Just in case you don't use upstream/:
+    branch_or_tag = branch_or_tag.replace("upstream/", upstream)
+    specs = []
+    for filename in filenames:
+        try:
+            blob = repo.git.show(f"{branch_or_tag}:{filename}")
+        except git.exc.GitCommandError:
+            specs.append("ø")
+        else:
+            specs.append(find_sphinx_spec(blob))
+    return specs
 
 
 CONF_FILES = {
     "travis": ".travis.yml",
-    "azure": ".azure-pipelines/docs-steps.yml",
     "requirements.txt": "Doc/requirements.txt",
     "conf.py": "Doc/conf.py",
 }
 
 
+def branch_or_tag_for(version):
+    if version.status == "EOL":
+        return f"tags/{version.branch_or_tag}"
+    return f"upstream/{version.branch_or_tag}"
+
+
 def search_sphinx_versions_in_cpython(repo: git.Repo):
     repo.git.fetch("https://github.com/python/cpython")
-    table = []
-    for version in sorted(build_docs.VERSIONS):
-        table.append(
-            [
-                version.name,
-                *[
-                    find_sphinx_in_file(repo, version.branch_or_tag, filename)
-                    for filename in CONF_FILES.values()
-                ],
-            ]
-        )
-    print(tabulate(table, headers=["version", *CONF_FILES.keys()], tablefmt="rst"))
+    filenames = CONF_FILES.values()
+    table = [
+        [
+            version.name,
+            *find_sphinx_in_files(repo, branch_or_tag_for(version), filenames),
+        ]
+        for version in VERSIONS
+    ]
+    headers = ["version", *CONF_FILES.keys()]
+    print(tabulate(table, headers=headers, tablefmt="rst", disable_numparse=True))
 
 
-async def get_version_in_prod(language, version):
-    url = f"https://docs.python.org/{language}/{version}/".replace("/en/", "/")
+async def get_version_in_prod(language: str, version: str) -> str:
+    if language == "en":
+        url = f"https://docs.python.org/{version}/"
+    else:
+        url = f"https://docs.python.org/{language}/{version}/"
     async with httpx.AsyncClient() as client:
         try:
-            response = await client.get(url, timeout=10)
+            response = await client.get(url, timeout=5)
         except httpx.ConnectTimeout:
             return "(timeout)"
-    text = response.text.encode("ASCII", errors="ignore").decode("ASCII")
+    # Python 2.6--3.7: sphinx.pocoo.org
+    # from Python 3.8: www.sphinx-doc.org
     if created_using := re.search(
-        r"(?:sphinx.pocoo.org|www.sphinx-doc.org).*?([0-9.]+[0-9])", text, flags=re.M
+        r"(?:sphinx.pocoo.org|www.sphinx-doc.org).*?([0-9.]+[0-9])", response.text
     ):
         return created_using.group(1)
     return "ø"
 
 
 async def which_sphinx_is_used_in_production():
-    table = []
-    for version in sorted(build_docs.VERSIONS):
-        table.append(
-            [
-                version.name,
-                *await asyncio.gather(
-                    *[
-                        get_version_in_prod(language.tag, version.name)
-                        for language in build_docs.LANGUAGES
-                    ]
-                ),
-            ]
-        )
-    print(
-        tabulate(
-            table,
-            disable_numparse=True,
-            headers=[
-                "version",
-                *[language.tag for language in sorted(build_docs.LANGUAGES)],
-            ],
-            tablefmt="rst",
-        )
-    )
+    table = [
+        [
+            version.name,
+            *await asyncio.gather(
+                *[
+                    get_version_in_prod(language.tag, version.name)
+                    for language in LANGUAGES
+                ]
+            ),
+        ]
+        for version in VERSIONS
+    ]
+    headers = ["version", *[language.tag for language in LANGUAGES]]
+    print(tabulate(table, headers=headers, tablefmt="rst", disable_numparse=True))
 
 
 def main():
     logging.basicConfig(level=logging.INFO)
     logging.getLogger("charset_normalizer").setLevel(logging.WARNING)
     logging.getLogger("asyncio").setLevel(logging.WARNING)
+    logging.getLogger("httpx").setLevel(logging.WARNING)
     args = parse_args()
     repo = git.Repo(args.cpython_clone)
     print("Sphinx configuration in various branches:", end="\n\n")
