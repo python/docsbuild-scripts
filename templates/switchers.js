7e274eb87a36acf9ca35e3f1671ee4eb2d87d9f3
Adam Turner <9087854+aa-turner@users.noreply.github.com>
2024-10-28 13:47:52 +0000

    Remove String.startsWith polyfill

--- templates/switchers.js
+++ templates/switchers.js
@@ -1,15 +1,6 @@
 (function() {
   'use strict';
 
-  if (!String.prototype.startsWith) {
-    Object.defineProperty(String.prototype, 'startsWith', {
-      value: function(search, rawPos) {
-        const pos = rawPos > 0 ? rawPos|0 : 0;
-        return this.substring(pos, pos + search.length) === search;
-      }
-    });
-  }
-
   // Parses versions in URL segments like:
   // "3", "dev", "release/2.7" or "3.6rc2"
   const version_regexs = [
