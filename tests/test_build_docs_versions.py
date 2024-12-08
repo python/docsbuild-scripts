6979d6c1593822c3f82ff3878f64c53f9c6a8d1e
Hugo van Kemenade <1324225+hugovk@users.noreply.github.com>
2024-12-08 20:31:34 +0200

    Add a test for Version.filter with default branch

--- tests/test_build_docs_versions.py
+++ tests/test_build_docs_versions.py
@@ -1,7 +1,29 @@
 from build_docs import Versions, Version
 
 
-def test_filter() -> None:
+def test_filter_default() -> None:
+    # Arrange
+    versions = [
+        Version("3.14", status="feature"),
+        Version("3.13", status="bugfix"),
+        Version("3.12", status="bugfix"),
+        Version("3.11", status="security"),
+        Version("3.10", status="security"),
+        Version("3.9", status="security"),
+    ]
+
+    # Act
+    filtered = Version.filter(versions)
+
+    # Assert
+    assert filtered == [
+        Version("3.14", status="feature"),
+        Version("3.13", status="bugfix"),
+        Version("3.12", status="bugfix"),
+    ]
+
+
+def test_filter_one() -> None:
     # Arrange
     versions = Versions([
         Version("3.14", status="feature"),
