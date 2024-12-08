6f913e3bfb8f2513b04c968e0344997f31458295
Hugo van Kemenade <1324225+hugovk@users.noreply.github.com>
2024-12-08 20:54:08 +0200

    Allow passing multiple branches build via CLI

--- tests/test_build_docs_version.py
+++ tests/test_build_docs_version.py
@@ -35,7 +35,28 @@ def test_filter_one() -> None:
     ]
 
     # Act
-    filtered = Version.filter(versions, "3.13")
+    filtered = Version.filter(versions, ["3.13"])
 
     # Assert
     assert filtered == [Version("3.13", status="security")]
+
+
+def test_filter_multiple() -> None:
+    # Arrange
+    versions = [
+        Version("3.14", status="feature"),
+        Version("3.13", status="bugfix"),
+        Version("3.12", status="bugfix"),
+        Version("3.11", status="security"),
+        Version("3.10", status="security"),
+        Version("3.9", status="security"),
+    ]
+
+    # Act
+    filtered = Version.filter(versions, ["3.13", "3.14"])
+
+    # Assert
+    assert filtered == [
+        Version("3.14", status="feature"),
+        Version("3.13", status="security"),
+    ]
