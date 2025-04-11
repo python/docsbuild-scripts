08cf923cb82f89687547505c209176e8a1438684
Hugo van Kemenade <1324225+hugovk@users.noreply.github.com>
2025-04-11 13:50:53 +0300

    Add a test for Versions.filter for single version

--- tests/test_build_docs_versions.py
+++ tests/test_build_docs_versions.py
@@ -1,19 +1,19 @@
-from build_docs import Version
+from build_docs import Versions, Version
 
 
 def test_filter() -> None:
     # Arrange
-    versions = [
+    versions = Versions([
         Version("3.14", status="feature"),
         Version("3.13", status="bugfix"),
         Version("3.12", status="bugfix"),
         Version("3.11", status="security"),
         Version("3.10", status="security"),
         Version("3.9", status="security"),
-    ]
+    ])
 
     # Act
-    filtered = Version.filter(versions, "3.13")
+    filtered = versions.filter("3.13")
 
     # Assert
     assert filtered == [Version("3.13", status="security")]
