a17b4c3a7a3daf9be0e5f6edc3669a8423d14cda
Hugo van Kemenade <1324225+hugovk@users.noreply.github.com>
2025-04-13 13:54:22 +0300

    Refactor test data into fixture

--- tests/test_build_docs_versions.py
+++ tests/test_build_docs_versions.py
@@ -1,9 +1,13 @@
+from __future__ import annotations
+
+import pytest
+
 from build_docs import Version, Versions
 
 
-def test_filter_default() -> None:
-    # Arrange
-    versions = Versions([
+@pytest.fixture
+def versions() -> Versions:
+    return Versions([
         Version(name="3.14", status="in development", branch_or_tag=""),
         Version(name="3.13", status="stable", branch_or_tag=""),
         Version(name="3.12", status="stable", branch_or_tag=""),
@@ -12,6 +16,8 @@ def test_filter_default() -> None:
         Version(name="3.9", status="security-fixes", branch_or_tag=""),
     ])
 
+
+def test_filter_default(versions) -> None:
     # Act
     filtered = versions.filter()
 
@@ -23,17 +29,7 @@ def test_filter_default() -> None:
     ]
 
 
-def test_filter_one() -> None:
-    # Arrange
-    versions = Versions([
-        Version(name="3.14", status="in development", branch_or_tag=""),
-        Version(name="3.13", status="stable", branch_or_tag=""),
-        Version(name="3.12", status="stable", branch_or_tag=""),
-        Version(name="3.11", status="security-fixes", branch_or_tag=""),
-        Version(name="3.10", status="security-fixes", branch_or_tag=""),
-        Version(name="3.9", status="security-fixes", branch_or_tag=""),
-    ])
-
+def test_filter_one(versions) -> None:
     # Act
     filtered = versions.filter(["3.13"])
 
@@ -41,17 +37,7 @@ def test_filter_one() -> None:
     assert filtered == [Version(name="3.13", status="security-fixes", branch_or_tag="")]
 
 
-def test_filter_multiple() -> None:
-    # Arrange
-    versions = Versions([
-        Version(name="3.14", status="in development", branch_or_tag=""),
-        Version(name="3.13", status="stable", branch_or_tag=""),
-        Version(name="3.12", status="stable", branch_or_tag=""),
-        Version(name="3.11", status="security-fixes", branch_or_tag=""),
-        Version(name="3.10", status="security-fixes", branch_or_tag=""),
-        Version(name="3.9", status="security-fixes", branch_or_tag=""),
-    ])
-
+def test_filter_multiple(versions) -> None:
     # Act
     filtered = versions.filter(["3.13", "3.14"])
 
